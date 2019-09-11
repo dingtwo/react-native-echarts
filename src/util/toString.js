08b11bd29df5560b7301a9d2ffffc6af41ecf11b
dingjz <dingjz>
2019-09-11 10:12:03 +0800

    csx-native-echarts

--- src/util/toString.js
+++ src/util/toString.js
@@ -9,5 +9,7 @@ export default function toString(obj) {
     do {
         result = result.replace('\"~--demo--~', '').replace('~--demo--~\"', '').replace(/\\n/g, '').replace(/\\\"/g,"\"");//最后一个replace将release模式中莫名生成的\"转换成"
     } while (result.indexOf('~--demo--~') >= 0);
+    // 添加此行把unicode转为中文，目前最省心的方法，其他方法稍麻烦
+    result = unescape(result.replace(/\\u/g, "%u"));
     return result;
 }
